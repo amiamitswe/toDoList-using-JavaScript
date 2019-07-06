117a0ce8604c0b9ed7547ba639f3c2ec84b283e4
Amit Samadder <amiamitswe@gmail.com>
2019-07-06 19:17:06 +0600

    add more functions for toDoList

--- asset/script.js
+++ asset/script.js
@@ -1,9 +1,32 @@
+
+
 let toDoList = {
     toDo: [],
     counter: 0,
     errorMsg: ""
 };
 
+// on body load function
+
+const on_Load_Body_Func = () => {
+    const json = localStorage.getItem('index');
+    document.querySelector("#input-text").value = "";
+
+    if(json) {
+        toDoList.toDo = [...JSON.parse(json)];
+
+        console.log("data loaded...");
+
+        load_Content_Of_To_Do_List();
+    }
+
+    else {
+        console.log('No data exist...');
+    }
+};
+
+
+// onType in input field function
 
 const onTypeText = () => {
     let button_value = document.querySelector("#submit_button");
@@ -14,27 +37,100 @@ const onTypeText = () => {
     document.querySelector(".error-box").classList.remove('show_alert');
 };
 
-
+// save value on press save me button
 
 const saveUserInput = () => {
-    let getInput = document.querySelector("#input-text").value;
+    let getInput = document.querySelector("#input-text").value.trim().toLowerCase();
 
     if (getInput !== "") {
+
+        if(toDoList.toDo.indexOf(getInput) > -1) {
+           return errorMessage("this choice already exist !!!");
+        }
+
         toDoList.toDo.push(getInput);
         document.querySelector("#input-text").value = "";
-        console.log(toDoList.toDo);
-    }
 
-    else {
-        toDoList.errorMsg = "Please type something";
+        let json = JSON.stringify(toDoList.toDo);
+        localStorage.setItem('index', json);
 
-        document.querySelector('#error_message').innerHTML = toDoList.errorMsg;
+        console.log("New data recorded "+ toDoList.toDo.length);
 
-        document.querySelector(".error-box").classList.add('show_alert');
+        load_Content_Of_To_Do_List();
 
-        let button_value = document.querySelector("#submit_button");
-        button_value.classList.add('error_msg');
-        button_value.disabled = true;
-        button_value.innerHTML = "Error";
+    } else {
+        errorMessage("please provide Your Choice");
     }
 };
+
+// delete all document on click delete all button
+
+const deleteAllRecords = () => {
+    let json = '';
+    localStorage.setItem('index', json);
+    toDoList.toDo = [];
+
+    console.log("Removed all records");
+
+    if (toDoList.toDo.length === 0) {
+        document.querySelector(".content-status").innerHTML = "<h2 class=\"no-content\">No Content Here</h2>";
+        document.querySelector(".all_options").innerHTML = "";
+    }
+};
+
+// what should I do now on your job now button
+
+const yourJobNow = () => {
+    document.querySelector(".modal").style.visibility = "visible";
+    document.querySelector(".container").style.opacity = "0.3";
+    document.querySelector(".container").style.zIndex = "-1";
+
+    const random_number = Math.floor(Math.random()* (toDoList.toDo.length));
+
+    document.querySelector('#current_task_is').innerHTML =  toDoList.toDo[random_number];
+};
+
+// close modal
+
+const closeModal = () => {
+    document.querySelector(".modal").style.visibility = "hidden";
+    document.querySelector(".container").style.opacity = "1";
+    document.querySelector(".container").style.zIndex = "1";
+};
+
+// view list to display function
+
+const load_Content_Of_To_Do_List = () => {
+    if (toDoList.toDo.length !== 0) {
+        let options = document.querySelector(".all_options");
+
+        document.querySelector(".content-status").innerHTML = (
+            "<button class='total-count'>Total : "+ toDoList.toDo.length +"</button>" +
+            "<button onclick='yourJobNow()' class='current-job'>Your Job Now !!!</button>" +
+            "<button onclick='deleteAllRecords()' class='delete-all'>Delete All <i class=\"material-icons delete-all-icon\">delete</i></button>"
+        );
+
+        options.innerHTML = toDoList.toDo.map((option, index) => {
+            return ('<li>' + (index + 1) + ". " + option + '<span><i class="material-icons edit-icon">edit</i><i class="material-icons delete-icon">delete</i></span>' + '</li>');
+        }).join(" ");
+    } else {
+        document.querySelector(".content-status").innerHTML = "<h2 class=\"no-content\">No Content Here</h2>";
+    }
+};
+
+
+// show error message function
+
+const errorMessage = (msg) => {
+    toDoList.errorMsg = msg;
+
+    document.querySelector('#error_message').innerHTML = toDoList.errorMsg;
+
+    document.querySelector(".error-box").classList.add('show_alert');
+
+    let button_value = document.querySelector("#submit_button");
+    button_value.classList.add('error_msg');
+    button_value.disabled = true;
+    button_value.innerHTML = "Error";
+};
+
